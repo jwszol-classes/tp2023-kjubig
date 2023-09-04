8d5e9c44da6fc665bc6ead1c837f6af4b7755f3e
kjubig <143036222+kjubig@users.noreply.github.com>
2023-09-04 02:25:42 +0200

    KOŃCOWE DODANIE OSÓB I PRACA NAD LOGIKĄ RUCHU WINDY
    Dodanie osób, które pojawią się na określonych piętrach z numerem docelowego piętra, na które mają się przemieścić. 
    Dalsza praca nad logiką ruchu windy, aby działała poprawnie.

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ public:
         {
             sf::RectangleShape tmp;
             tmp.setSize({25,25});
-            tmp.setFillColor(sf::Color::Cyan);
+            tmp.setFillColor(sf::Color::Yellow);
             buttons.emplace_back(tmp);
         }
     }
@@ -57,13 +57,35 @@ public:
     int waga = 70;
     sf::Texture texture;
     sf::Sprite body;
+    sf::Text OznaczenieOsoby;
+    sf::Font font;
+    //sf font sf text
+
 
 
     Osoba(float doPietra) : pietroDo((int)doPietra)
     {
-        texture.loadFromFile("./stick.png");
+        texture.loadFromFile("./corpo.png");
         body.setTexture(texture);
         body.setScale(0.2,0.2);
+        //ustawiasz font i text pamietaj o pozycji tez wzgledem osoby na pietrze
+
+        font.loadFromFile("Oswald.ttf");
+
+        OznaczenieOsoby.setFont(font);
+        OznaczenieOsoby.setCharacterSize(24);
+        OznaczenieOsoby.setFillColor(sf::Color::Black);
+        OznaczenieOsoby.setOutlineThickness(3);
+        OznaczenieOsoby.setOutlineColor(sf::Color::Yellow);
+        OznaczenieOsoby.setString(to_string((int)doPietra));
+
+    }
+
+
+    void setPosition(int x, int y)
+    {
+        body.setPosition(x,y);
+        OznaczenieOsoby.setPosition(x+42,y+40);
     }
     //    Osoba(float doPietra, int wag) : pietroDo((int)doPietra), waga(wag)
     //    {
@@ -71,6 +93,12 @@ public:
     //        body.setTexture(texture);
     //        body.setScale(0.1,0.1);
     //    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(body);
+        window.draw(OznaczenieOsoby);
+    }
 };
 
 class Pietro
@@ -81,13 +109,14 @@ public:
     vector<Osoba*> kolejka;
     ButtonGroup buttons;
 
+
     Pietro()
     {
         texture.loadFromFile("./pietro.jpg");
         body.setTexture(texture);
         body.setScale(0.29f, 0.25f);
 
-        //        button.setFillColor(sf::Color::Green);
+
     }
 
     ~Pietro()
@@ -103,12 +132,12 @@ public:
         return body.getPosition();
     }
 
-    void add(int nr)
+    void add(int nr) //dodawanie osob do pietra
     {
         kolejka.emplace_back(new Osoba(nr));
         for(int i = 0; i < (int)kolejka.size(); i++)
         {
-            kolejka[i]->body.setPosition(175 + i*200, 100);
+            kolejka[i]->setPosition(225 + i*50, body.getPosition().y + 60); //dopasuj przesuniecie na x, przesuniecie na y oraz odstepy na x miedzy ludzmi
         }
     }
 
@@ -119,7 +148,7 @@ public:
         buttons.draw(window);
         for(int i = 0; i < (int)kolejka.size(); i++)
         {
-            window.draw(kolejka[i]->body);
+            kolejka[i]->draw(window);
         }
     }
 };
@@ -181,6 +210,8 @@ public:
     sf::Sprite shaftSprite;
     sf::Texture floorbackgroundTexture;
     sf::Sprite floorbackgroundSprite;
+    vector<int> kolejkaPieter;
+    sf::Vector2f maxMinPietro = {0,4};
 
     SystemWindy(int iloscPieter, sf::RenderWindow &window)
     {
@@ -211,6 +242,38 @@ public:
         }
     }
 
+    void dodajDoKolejkiPieter(int pietro)
+    {
+//        if (winda.kierunek == Idle)
+//        {
+//            winda.kierunek = Up;
+//            if (winda.nrPietra > pietro)
+//            {
+//                if (pietro < maxMinPietro.y)
+//                    maxMinPietro.y = pietro;
+//            }
+//        }
+        if (winda.kierunek == Up)
+        {
+            if (winda.nrPietra > pietro)
+            {
+                if (pietro < maxMinPietro.y)
+                    maxMinPietro.y = pietro;
+            }
+        }
+        else if (winda.kierunek == Down)
+        {
+            if (winda.nrPietra < pietro)
+            {
+                if (pietro > maxMinPietro.x)
+                    maxMinPietro.x = pietro;
+            }
+        }
+        cout << winda.kierunek << " " << winda.cel << " " << winda.nrPietra << endl;
+        moveWinda(pietro);
+        cout << winda.kierunek << " " << winda.cel << " " << winda.nrPietra << endl;
+    }
+
     void events(sf::RenderWindow &window, sf::Event &event)
     {
         //eventy do guzikow pieter:
@@ -221,7 +284,8 @@ public:
                 if (pietra[i]->buttons.buttons[j].getGlobalBounds().contains(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y) && event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
                 {
                     if (i != j) pietra[i]->add(j);
-                    moveWinda(j);
+                    //moveWinda(j);
+                    dodajDoKolejkiPieter(j);
                 }
             }
         }
@@ -241,41 +305,85 @@ public:
         }
     }
 
-//    void moveWinda(int pietro)
-//    {
-//        winda.setPosition(pietra[pietro]->getPosition().y);
-//        winda.nrPietra = pietro;
-//        cout << "winda ruszyla sie na pietro: " << pietro << endl;
-//    }
+    //    void moveWinda(int pietro)
+    //    {
+    //        winda.setPosition(pietra[pietro]->getPosition().y);
+    //        winda.nrPietra = pietro;
+    //        cout << "winda ruszyla sie na pietro: " << pietro << endl;
+    //    }
 
-    void moveWinda(int pietro)
+    void moveWinda(bool czekac)
     {
 
         //winda.setPosition(pietra[pietro]->getPosition().y);
-        winda.cel = pietro;
-        if (winda.cel == winda.nrPietra)
+        //winda.cel = pietro;
+        if (!czekac)
         {
-            winda.setKierunek(Kierunek::Idle);
+            if (winda.cel == winda.nrPietra)
+            {
+                winda.setKierunek(Kierunek::Idle);
+            }
+            else if (winda.cel < winda.nrPietra )
+            {
+                winda.setKierunek(Kierunek::Down);
+            }
+            else if (winda.cel > winda.nrPietra )
+            {
+                winda.setKierunek(Kierunek::Up);
+            }
+            //cout << winda.cel << " " << winda.kierunek << endl;
+            //cout << "winda ruszyla sie na pietro: " << pietro << endl;
+            //if()
         }
-        else if (winda.cel < winda.nrPietra )
-        {
-            winda.setKierunek(Kierunek::Down);
-        }
-        else if (winda.cel > winda.nrPietra )
-        {
-            winda.setKierunek(Kierunek::Up);
-        }
-        cout << winda.cel << " " << winda.kierunek << endl;
-        //cout << "winda ruszyla sie na pietro: " << pietro << endl;
-        //if()
+        else winda.speed = 0;
+
     }
 
     void test()
     {
+
+        //Gdy dojezdzamy do najwyzszego najniszego pietra ktory mozemy zeby odebrac ludzi
         if (winda.nrPietra == winda.cel)
         {
-            winda.setKierunek(Idle);
+            if (winda.kierunek == Up)
+            {
+                maxMinPietro.x = 0;
+                if (maxMinPietro.y < 4)
+                {
+                    winda.cel = maxMinPietro.y;
+                    winda.setKierunek(Down);
+                }
+            }
+            else if (winda.kierunek == Down)
+            {
+                maxMinPietro.y = 4;
+                if (maxMinPietro.x > 0)
+                {
+                    winda.cel = maxMinPietro.x;
+                    winda.setKierunek(Up);
+                }
+            }
+            if (maxMinPietro.x == 0 && maxMinPietro.y == 4)
+                winda.setKierunek(Idle);
         }
+
+        //update celu na podstawie wartosci min max ktora mogla sie zmienic
+        if (winda.kierunek == Up)
+        {
+            if (maxMinPietro.x > 0)
+                winda.cel = maxMinPietro.x;
+        }
+        else if (winda.kierunek == Down)
+        {
+            if (maxMinPietro.y < 4)
+                winda.cel = maxMinPietro.y;
+        }
+
+        moveWinda((int)(winda.nrPietra*10) % 10 == 0); //operator a == b
+        //powyzsza funkcja zatrzymuj na pietrze poniewaz jest na pietrze
+        //dlatego bedzie potrzebny drugi warunek ktory np bedzie sprawdzal czy sa osoby na pietrze
+
+        //update current pietra na ktorym jestesmy wobec pozycji xy windy wzgledem pieterr (oraz ich niewidzialnych prostokatow)
         for(int i = 0; i < pietra.size(); i++)
         {
             if (winda.body.getPosition().y > pietra[pietra.size()-1-i]->body.getPosition().y - 5 && winda.body.getPosition().y < pietra[pietra.size()-1-i]->body.getPosition().y + 5)
@@ -302,41 +410,41 @@ public:
         }
 
         //                {
-//        for(int i = 0; i < pietra.size(); i++)
-//        {
-//            if (!pietra[i]->kolejka.empty())
-//            {
-//                if (winda.kierunek == Kierunek::Idle)
-//                {
-//                    int tmp = abs(winda.nrPietra - i);
-//                    if (tmp == 0)
-//                        winda.setKierunek(Kierunek::Idle);
-//                    else if (tmp > 0)
-//                        winda.setKierunek(Kierunek::Down);
-//                    else if (tmp < 0)
-//                        winda.setKierunek(Kierunek::Up);
-//                }
-//            }
-//        }
+        //        for(int i = 0; i < pietra.size(); i++)
+        //        {
+        //            if (!pietra[i]->kolejka.empty())
+        //            {
+        //                if (winda.kierunek == Kierunek::Idle)
+        //                {
+        //                    int tmp = abs(winda.nrPietra - i);
+        //                    if (tmp == 0)
+        //                        winda.setKierunek(Kierunek::Idle);
+        //                    else if (tmp > 0)
+        //                        winda.setKierunek(Kierunek::Down);
+        //                    else if (tmp < 0)
+        //                        winda.setKierunek(Kierunek::Up);
+        //                }
+        //            }
+        //        }
 
-//                for(int i = 0; i < pietra.size(); i++)
-//                {
-//                    if (!pietra[i]->kolejka.empty())
-//                    {
-//                        if (winda.nrPietra != i) moveWinda(i);
-//                        while(!pietra[i]->kolejka.empty())
-//                        {
-//                            winda.pasazerowie.emplace_back(pietra[i]->kolejka[0]);
-//                            pietra[i]->kolejka.erase(pietra[i]->kolejka.begin());
-//                        }
-//                    }
-//                }
+        //                for(int i = 0; i < pietra.size(); i++)
+        //                {
+        //                    if (!pietra[i]->kolejka.empty())
+        //                    {
+        //                        if (winda.nrPietra != i) moveWinda(i);
+        //                        while(!pietra[i]->kolejka.empty())
+        //                        {
+        //                            winda.pasazerowie.emplace_back(pietra[i]->kolejka[0]);
+        //                            pietra[i]->kolejka.erase(pietra[i]->kolejka.begin());
+        //                        }
+        //                    }
+        //                }
 
-//                while(!winda.pasazerowie.empty())
-//                {
-//                    if (winda.nrPietra != winda.pasazerowie[0]->pietroDo) moveWinda(winda.pasazerowie[0]->pietroDo);
-//                    winda.pasazerowie.erase(winda.pasazerowie.begin());
-//                }
+        //                while(!winda.pasazerowie.empty())
+        //                {
+        //                    if (winda.nrPietra != winda.pasazerowie[0]->pietroDo) moveWinda(winda.pasazerowie[0]->pietroDo);
+        //                    winda.pasazerowie.erase(winda.pasazerowie.begin());
+        //                }
     }
 
     void drawScreen(sf::RenderWindow &window)
@@ -373,10 +481,10 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
             systemWindy.events(window, event);
-//            if (event.type == sf::Event::MouseMoved)
-//            {
-//                systemWindy.winda.setPosition(sf::Mouse::getPosition(window).y);
-//            }
+            //            if (event.type == sf::Event::MouseMoved)
+            //            {
+            //                systemWindy.winda.setPosition(sf::Mouse::getPosition(window).y);
+            //            }
             if (event.type == sf::Event::MouseButtonPressed)
             {
                 for(int i = 0; i < systemWindy.pietra.size(); i++)
@@ -398,6 +506,7 @@ int main()
         }
         systemWindy.test();
         systemWindy.windaMovement();
+
         window.clear();
         systemWindy.drawScreen(window);
         window.display();
@@ -407,37 +516,37 @@ int main()
 }
 
 
-int main1()
-{
-    sf::RenderWindow window(sf::VideoMode(1200, 900), "Elevator");
-    sf::RectangleShape rect({50,50});
-    window.setFramerateLimit(60);
-    rect.setPosition(300,300);
-    sf::Clock timer;
-    int kierunek = 0;
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::Up)
-                kierunek = -1;
-            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::Down)
-                kierunek = 1;
-            else
-                kierunek = 0;
-        }
-        if(timer.getElapsedTime().asMilliseconds() >= 1)
-        {
-            rect.move(0,kierunek);
-            timer.restart();
-        }
-        window.clear();
-        window.draw(rect);
-        window.display();
-    }
+//int main1()
+//{
+//    sf::RenderWindow window(sf::VideoMode(1200, 900), "Elevator");
+//    sf::RectangleShape rect({50,50});
+//    window.setFramerateLimit(60);
+//    rect.setPosition(300,300);
+//    sf::Clock timer;
+//    int kierunek = 0;
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::Up)
+//                kierunek = -1;
+//            else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Key::Down)
+//                kierunek = 1;
+//            else
+//                kierunek = 0;
+//        }
+//        if(timer.getElapsedTime().asMilliseconds() >= 1)
+//        {
+//            rect.move(0,kierunek);
+//            timer.restart();
+//        }
+//        window.clear();
+//        window.draw(rect);
+//        window.display();
+//    }
 
-    return 0;
-}
+//    return 0;
+//}
